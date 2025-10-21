e676065bc29152d7cc0e9d22f709fa70ebfa7a89
Marti Sabaté <sabatemiranda@gmail.com>
2025-10-21 11:12:08 +0200

    player controller and scripts folder

---
+++ IsoCombat/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+   
+    [Header("Health References")]
+    public float currentHealth = 0f;
+    public float maxHealth = 10f;
+    public float speed = 2;
+
+    [Header("Player References")]
+    public Transform playerSprite;
+    public Transform playerGraphics;
+
+
+    [Header("Particles References")]
+    public ParticleSystem dieParticles;
+    public ParticleSystem dieCircleParticles;
+
+    [Header("Challenge References")]
+    public TextMeshProUGUI challengeTextIndicator;
+    public Canvas darknessCanvasChallenge;
+
+    
+    //Movement
+    private Camera mainCamera;
+    private Vector2 targetPosition;
+    private float screenMargin = 0.5f;
+
+    //Timer
+    private float timeCounter = 0;
+
+    //Invulnerability
+    [HideInInspector] public float invulnerabilityTimer = 0f;
+    [HideInInspector] public float invulnerabilityCoolDownTimer = 0f;
+    [HideInInspector] public float invulnerabilityCoolDown = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        targetPosition = transform.position;
+        timeCounter = 0;
+        currentHealth = maxHealth;
+        playerSprite.GetComponent<SpriteRenderer>().material.SetFloat("_FillAmount", currentHealth / maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            PlayerDie();
+        }
+
+        UpdateTime();
+        UpdateMovement();
+        UpdateInvulnerability();
+
+
+    }
+
+    
+    void UpdateMovement()
+    {
+        Vector2 currentPosition = transform.position;
+        Vector2 newPosition = currentPosition;
+
+        Vector3 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+        newPosition = Vector2.MoveTowards(currentPosition, targetPosition, speed * Time.deltaTime);
+        
+
+
+
+        // --- Limitar dentro de la cámara ---
+        float camHeight = mainCamera.orthographicSize;
+        float camWidth = camHeight * mainCamera.aspect;
+
+        float minX = mainCamera.transform.position.x - camWidth + screenMargin;
+        float maxX = mainCamera.transform.position.x + camWidth - screenMargin;
+        float minY = mainCamera.transform.position.y - camHeight + screenMargin;
+        float maxY = mainCamera.transform.position.y + camHeight - screenMargin;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
+        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
+
+        transform.position = newPosition;
+    }
+
+    void UpdateInvulnerability()
+    {
+        bool inputActivado = Input.GetMouseButtonDown(1) || Input.GetAxis("Fire2") > 0.1f;
+
+        
+    }
+
+
+
+    void PlayerDie()
+    {
+        GetComponent<CircleCollider2D>().enabled = false;
+        playerGraphics.gameObject.SetActive(false);
+        dieParticles.Emit(35);
+        dieCircleParticles.Emit(1);
+        //CameraShake.Instance.ShakeCamera(0.3f, 0.2f);
+    }
+
+    void UpdateTime()
+    {
+        timeCounter += Time.deltaTime;
+        //if (timeCounter >= PlayerRuntimeStats.instance.realTimeStats.currentMaxTime && !dieDoned) {
+        //    dieDoned = true;
+        //    PlayerDie();
+        //    CircleTransition.instance.CloseBlackScreen("AbilityTree");
+        //}
+        //timeImage.fillAmount = timeCounter / PlayerRuntimeStats.instance.realTimeStats.currentMaxTime;
+        //timeText.text = GetTimeString();
+        //PlayerRuntimeStats.instance.gameStats.elapsedTime = timeCounter;
+    }
+
+
+    public string GetTimeString()
+    {
+        //int totalSeconds = (Mathf.CeilToInt(PlayerRuntimeStats.instance.realTimeStats.currentMaxTime - timeCounter));
+
+        //int minutes = (totalSeconds / 60);
+        //int seconds = totalSeconds % 60;
+
+
+        //return string.Format("{0:00}:{1:00}", minutes, seconds);
+        return "";
+    }
+
+}

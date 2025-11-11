b14c0eefd1d84f1c8f790589f175c69dc22c6cd5
Aleix3 <Aleixbotella.1@gmail.com>
2025-11-11 11:21:01 +0100

    .

--- IsoCombat/Assets/Scripts/Player/PlayerColliderPart.cs
+++ IsoCombat/Assets/Scripts/Player/PlayerColliderPart.cs
@@ -5,6 +5,7 @@ public class PlayerColliderPart : MonoBehaviour
     public enum PartType { Upper, Lower }
     public PartType partType;
     [HideInInspector] public PlayerController owner;
+    public float bounceForce = 5f;
 
     void Start()
     {
@@ -18,12 +19,23 @@ public class PlayerColliderPart : MonoBehaviour
 
         if (otherPart.owner == owner) return;
 
+        Rigidbody2D rbOwner = owner.GetComponent<Rigidbody2D>();
+        Rigidbody2D rbOther = otherPart.owner.GetComponent<Rigidbody2D>();
+
         if (partType == PartType.Lower && otherPart.partType == PartType.Upper)
         {
             Debug.Log($"{owner.name} golpeó a {otherPart.owner.name}");
             owner.TakeDamage(1);
+        }
 
+        if (rbOwner != null && rbOther != null)
+        {
+            // Dirección normal del impacto
+            Vector2 normal = collision.contacts[0].normal;
 
+            // Aplicar fuerza opuesta a cada uno
+            rbOwner.AddForce(-normal * bounceForce, ForceMode2D.Impulse);
+            rbOther.AddForce(normal * bounceForce, ForceMode2D.Impulse);
         }
 
 
